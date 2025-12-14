f928eedc835b255bcf689438561d4d2d727d310c
Mitchell Herndon <mherndon2010@gmail.com>
2025-12-13 18:49:52 -0600

    Added ability to select tools

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  protected readonly activeTool: WritableSignal<string>;
 
+  constructor() {
+    this.activeTool = signal('home');
+  }
+
+  protected onActive(tool: string): void {
+    this.activeTool.set(tool);
+  }
 }
