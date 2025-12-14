91ce5ff61614afe4672c002224446a892e278593
Mitchell Herndon <mherndon2010@gmail.com>
2025-12-13 19:26:26 -0600

    Separated collapsing and collapsed event around the transition animation time

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, effect, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,11 +8,27 @@ import { Component, signal, WritableSignal } from '@angular/core';
 })
 export class SidebarComponent {
   protected readonly activeTool: WritableSignal<string>;
+  protected readonly collapsing: WritableSignal<boolean>;
   protected readonly collapsed: WritableSignal<boolean>;
+
+  protected hideTextTimer?: number;
 
   constructor() {
     this.activeTool = signal('home');
+    this.collapsing = signal(false);
     this.collapsed = signal(false);
+
+    effect(() => {
+      if (this.collapsing()) {
+        clearTimeout(this.hideTextTimer);
+        this.hideTextTimer = setTimeout(() => {
+          this.collapsed.set(true);
+        }, 200);
+      } else {
+        clearTimeout(this.hideTextTimer);
+        this.collapsed.set(false);
+      }
+    });
   }
 
   protected onActive(tool: string): void {
