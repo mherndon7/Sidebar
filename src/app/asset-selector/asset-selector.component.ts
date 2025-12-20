1dd4c29c11d66e166ec42a0607eb8811c2bdf7ab
Mitchell Herndon <mherndon2010@gmail.com>
2025-12-19 18:16:52 -0600

    Added typeahead for asset selector

--- src/app/asset-selector/asset-selector.component.ts
+++ src/app/asset-selector/asset-selector.component.ts
@@ -1,12 +1,44 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  Observable,
+  OperatorFunction,
+  Subject,
+  merge,
+} from 'rxjs';
 
 @Component({
   selector: 'app-asset-selector',
-  imports: [],
+  imports: [NgbTypeahead, FormsModule],
   templateUrl: './asset-selector.component.html',
   styleUrl: './asset-selector.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AssetSelectorComponent {
+  @ViewChild('instance', { static: true }) instance!: NgbTypeahead;
 
+  readonly assets: string[] = ['Model 1', 'Model 22', 'Model 3', 'Model 4', 'Model 5', 'unique'];
+  protected asset: string = '';
+
+  protected focus$ = new Subject<string>();
+  protected click$ = new Subject<string>();
+
+  protected search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(0), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+
+    return merge(debouncedText$, this.focus$, clicksWithClosedPopup$).pipe(
+      map((term: string) =>
+        (term === ''
+          ? this.assets
+          : this.assets.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1)
+        ).slice(0, 10),
+      ),
+    );
+  };
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { AssetSelectorComponent } from '../asset-selector/asset-selector.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [AssetSelectorComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
